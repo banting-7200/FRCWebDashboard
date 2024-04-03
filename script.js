d571be9f2824445f3a81be620bc1fc6e5b7211c6
jsrii <jas.kanthan@gmail.com>
2024-04-02 21:51:13 -0400

    Animations, coloring based on whether rank has gone up or down

--- script.js
+++ script.js
@@ -17,24 +17,54 @@ let telePoints = document.getElementById('telePoints');
 let endgamePoints = document.getElementById('endgamePoints');
 
 
+// previous Data
+
+let previousRank = -1;
+let previousWins = -1;
+let previousTies = -1;
+let previousLosses = -1;
+let previousEpa = -1;
+let previousAutoPoints = -1;
+let previoustelePoints = -1;
+let previousEndgamePoints = -1;
+
+// Icons
+
+let sameRankIcon = document.getElementById('sameRank');
+let higherRankIcon = document.getElementById('higherRank');
+let lowerRankIcon = document.getElementById('lowerRank');
+
 
 
 
 
 document.addEventListener("DOMContentLoaded", function () {
-    resizeRightHand();
-    window.addEventListener("resize", resizeRightHand);
+    resizestatsInfo();
+    window.addEventListener("resize", resizestatsInfo);
 
-    function resizeRightHand() {
-        var leftHandHeight = document.querySelector(".leftHand").offsetHeight;
-        document.querySelector(".rightHand").style.height = leftHandHeight + "px";
+    function resizestatsInfo() {
+        var generalInfoHeight = document.querySelector(".generalInfo").offsetHeight;
+        document.querySelector(".statsInfo").style.height = generalInfoHeight + "px";
     }
 });
 
 document.getElementById('reloadApp').addEventListener('click', function () {
-    getTeamData(teamNumber.value, yearOfComp.value, eventName.value);
+    getTeamData(teamNumber.value, yearOfComp.value, eventName.value)
 
-})
+});
+
+document.getElementById('refreshApp').addEventListener('click', function () {
+    getTeamData(teamNumber.value, yearOfComp.value, eventName.value)
+
+});
+
+
+
+
+
+setInterval(() => {
+    getTeamData(teamNumber.value, yearOfComp.value, eventName.value);
+}, 600000);
 
 function getTeamData(teamNumber, yearNumber, eventName) {
     const apiKey = ' ZYBxNxrdFx8PfRxwTj5awXIFyWCsR9Rz1xkunI9KiPq7GDn4g5bU25KKGKyeqQTO ';
@@ -81,18 +111,34 @@ function getTeamData(teamNumber, yearNumber, eventName) {
 
 
 function setBlueAlliance(data) {
+    console.log(data);
     teamNum.innerHTML = teamNumber.value;
-    currentRank.innerHTML = data.qual.ranking.rank;
+
+    checkRank(data);
+
     wins.innerHTML = data.qual.ranking.record.wins;
     ties.innerHTML = data.qual.ranking.record.ties;
     losses.innerHTML = data.qual.ranking.record.losses;
+    unfade(teamNum);
+    unfade(currentRank);
+    unfade(wins);
+    unfade(ties);
+    unfade(losses);
 }
 
 function setStatbotics(data) {
+    checkEPA(data);
+    checkAutoPoints(data);
+    checkTeleopPoints(data);
+    checkEndgamePoints(data);
     epa.value = data.epa.breakdown.total_points.mean;
     autoPoints.value = data.epa.breakdown.auto_points.mean;
     telePoints.value = data.epa.breakdown.teleop_points.mean;
     endgamePoints.value = data.epa.breakdown.endgame_points.mean;
+    unfade(epa);
+    unfade(autoPoints);
+    unfade(telePoints);
+    unfade(endgamePoints);
 }
 
 document.getElementById('modeSwitcher').addEventListener('click', () => {
@@ -103,3 +149,162 @@ document.getElementById('modeSwitcher').addEventListener('click', () => {
         document.documentElement.setAttribute('data-bs-theme', 'dark')
     }
 })
+
+
+function unfade(element) {
+    var op = 0.1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op >= 1) {
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.2;
+    }, 10);
+}
+
+
+function checkRank(data) {
+    if (previousRank != -1) {
+        if (previousRank > data.qual.ranking.rank) {
+            currentRank.innerHTML = data.qual.ranking.rank;
+            previousRank = data.qual.ranking.rank;
+            sameRankIcon.style.display = "none";
+            higherRankIcon.style.display = "none";
+            lowerRankIcon.style.display = "inline-block";
+        } else if (previousRank < data.qual.ranking.rank) {
+            currentRank.innerHTML = data.qual.ranking.rank;
+            previousRank = data.qual.ranking.rank;
+            sameRankIcon.style.display = "none";
+            higherRankIcon.style.display = "inline-block";
+            lowerRankIcon.style.display = "none";
+        } else {
+            currentRank.innerHTML = data.qual.ranking.rank;
+            sameRankIcon.style.display = "inline-block";
+            higherRankIcon.style.display = "none";
+            lowerRankIcon.style.display = "none";
+        }
+    } else {
+        currentRank.innerHTML = data.qual.ranking.rank;
+        sameRankIcon.style.display = "inline-block";
+        higherRankIcon.style.display = "none";
+        lowerRankIcon.style.display = "none";
+    }
+}
+
+
+function checkEPA(data) {
+    if (previousEpa != -1) {
+        if (previousEpa > data.epa.breakdown.total_points.mean) {
+            epa.innerHTML = data.epa.breakdown.total_points.mean;
+            previousEpa = data.epa.breakdown.total_points.mean;
+            setBackgrounds('identifierEPA', 'epa', 'down');
+        } else if (previousEpa < data.epa.breakdown.total_points.mean) {
+            epa.innerHTML = data.epa.breakdown.total_points.mean;
+            previousEpa = data.epa.breakdown.total_points.mean;
+            setBackgrounds('identifierEPA', 'epa', 'up');
+        } else {
+            epa.innerHTML = data.epa.breakdown.total_points.mean;
+        }
+    } else {
+        epa.innerHTML = data.epa.breakdown.total_points.mean;
+    }
+}
+
+function checkAutoPoints(data) {
+    if (previousAutoPoints != -1) {
+        if (previousAutoPoints > data.epa.breakdown.auto_points.mean) {
+            autoPoints.innerHTML = data.epa.breakdown.auto_points.mean;
+            previousAutoPoints = data.epa.breakdown.auto_points.mean;
+            setBackgrounds('identifierAuto', 'autoPoints', 'down');
+        } else if (previousAutoPoints < data.epa.breakdown.auto_points.mean) {
+            autoPoints.innerHTML = data.epa.breakdown.auto_points.mean;
+            previousAutoPoints = data.epa.breakdown.auto_points.mean;
+            setBackgrounds('identifierAuto', 'autoPoints', 'up');
+        } else {
+            autoPoints.innerHTML = data.epa.breakdown.auto_points.mean;
+        }
+    } else {
+        autoPoints.innerHTML = data.epa.breakdown.auto_points.mean;
+    }
+}
+
+function checkTeleopPoints(data) {
+    if (previoustelePoints != -1) {
+        if (previoustelePoints > data.epa.breakdown.teleop_points.mean) {
+            telePoints.innerHTML = data.epa.breakdown.teleop_points.mean;
+            previoustelePoints = data.epa.breakdown.teleop_points.mean;
+            setBackgrounds('identifierTeleop', 'telePoints', 'down');
+        } else if (previoustelePoints < data.epa.breakdown.teleop_points.mean) {
+            telePoints.innerHTML = data.epa.breakdown.teleop_points.mean;
+            previoustelePoints = data.epa.breakdown.teleop_points.mean;
+            setBackgrounds('identifierTeleop', 'telePoints', 'up');
+        } else {
+            telePoints.innerHTML = data.epa.breakdown.teleop_points.mean;
+        }
+    } else {
+        telePoints.innerHTML = data.epa.breakdown.teleop_points.mean;
+    }
+}
+
+function checkEndgamePoints(data) {
+    if (previousEndgamePoints != -1) {
+        if (previousEndgamePoints > data.epa.breakdown.endgame_points.mean) {
+            endgamePoints.innerHTML = data.epa.breakdown.endgame_points.mean;
+            previousEndgamePoints = data.epa.breakdown.endgame_points.mean;
+            setBackgrounds('identifierEndgame', 'endgamePoints', 'down');
+        } else if (previousEndgamePoints < data.epa.breakdown.endgame_points.mean) {
+            endgamePoints.innerHTML = data.epa.breakdown.endgame_points.mean;
+            previousEndgamePoints = data.epa.breakdown.endgame_points.mean;
+            setBackgrounds('identifierEndgame', 'endgamePoints', 'up');
+        } else {
+            endgamePoints.innerHTML = data.epa.breakdown.endgame_points.mean;
+        }
+    } else {
+        endgamePoints.innerHTML = data.epa.breakdown.endgame_points.mean;
+    }
+}
+
+
+
+function setBackgrounds(identifier, value, ranking) {
+    var descriptionID = document.getElementById(identifier);
+    var valueID = document.getElementById(value);
+
+    let rankingUp = ['bg-success-subtle', 'border-success'];
+    let rankingDown = ['bg-danger-subtle', 'border-danger'];
+
+    if (descriptionID && valueID) {
+        if (ranking == 'up') {
+            rankingDown.forEach(removedClass => {
+                descriptionID.classList.remove(removedClass);
+            });
+            rankingDown.forEach(removedClass => {
+                valueID.classList.remove(removedClass);
+            });
+            rankingUp.forEach(addedClass => {
+                descriptionID.classList.add(addedClass);
+            });
+            rankingUp.forEach(addedClass => {
+                valueID.classList.add(addedClass);
+            });
+        } else if (ranking == 'down') {
+            rankingUp.forEach(removedClass => {
+                descriptionID.classList.remove(removedClass);
+            });
+            rankingUp.forEach(removedClass => {
+                valueID.classList.remove(removedClass);
+            });
+
+
+            rankingDown.forEach(addedClass => {
+                descriptionID.classList.add(addedClass);
+            });
+            rankingDown.forEach(addedClass => {
+                valueID.classList.add(addedClass);
+            });
+        }
+    }
+
+}
+
