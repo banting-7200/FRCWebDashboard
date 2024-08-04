af95274c1675055d2c06527583812ff1c8f58526
arcthicc <jas.kanthan@gmail.com>
2024-08-04 09:38:10 -0400

    Spelling mistakes and nav icons

--- compareTeams.js
+++ compareTeams.js
@@ -1,14 +1,27 @@
-document.getElementById('qrCodeExport').style.color = "white";
+// Initializing functions
+initIcons();
+
+
+function initIcons() {
+    document.getElementById('qrCodeExport').style.color = "white";
+    document.getElementById('dashContainer').style.color = "white";
+    document.getElementById('matchContainer').style.color = "white";
+}
+
 
 document.getElementById('themeSwitcher').addEventListener('click', () => {
     if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
         document.documentElement.setAttribute('data-bs-theme', 'light')
         document.getElementById('qrCodeExport').style.color = "black";
+        document.getElementById('dashContainer').style.color = "black";
+        document.getElementById('matchContainer').style.color = "black";
         document.getElementById('themeSwitcherIcon').innerHTML = "dark_mode";
     }
     else {
         document.documentElement.setAttribute('data-bs-theme', 'dark')
         document.getElementById('qrCodeExport').style.color = "white";
+        document.getElementById('dashContainer').style.color = "white";
+        document.getElementById('matchContainer').style.color = "white";
         document.getElementById('themeSwitcherIcon').innerHTML = "light_mode";
     }
 });
